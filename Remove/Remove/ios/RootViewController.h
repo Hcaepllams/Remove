33c08a54e6d55454cbdac47f5ff41b3353dbb054
Hcaepllams <115678925@qq.com>
2013-08-21 17:34:10 +0800

    first commit

---
+++ Remove/Remove/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RemoveAppController.h
+//  Remove
+//
+//  Created by 陶 然景 on 13-8-21.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
